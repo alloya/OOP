ab931f46d0390ec2443edc6cb9ab02c692927a59
Yulia Lushinskaya <ylu@portia.ru>
2016-09-23 15:08:39 +0300

    Initial commit

---
+++ Lab1/Lab1/FindText/SearchText.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	if (argc != 3)
+	{
+		cout << "Invalid arguments count\n"
+			<< "Usage: findtext.exe <input file> <output file>\n";
+		return 1;
+	}
+
+	// Объявили переменную типа ifstream 
+	// (input file stream, поток для чтения из файла), проинициализировав его
+	// именем входного файла
+	ifstream input(argv[1]);
+
+	// вызываем метод is_open() у объекта input,
+	// который вернет true, если файл был открыт
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << argv[1] << " for reading\n";
+		return 1;
+	}
+
+	// создали поток для записи в выходной файл
+	ofstream output(argv[2]);
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << argv[2] << " for writing\n";
+		return 1;
+	}
+
+	char ch;
+	// Считываем в переменную ch символ из входного потока
+	// возвращаем значение, приводимое к bool, которое сигнализирует об успехе
+	while (input.get(ch))
+	{
+		if (!output.put(ch))
+		{
+			cout << "Failed to save data on disk\n";
+			return 1;
+		}
+	}
+
+	if (!output.flush()) // Если не удалось сбросить данные на диск
+	{
+		cout << "Failed to save data on disk\n";
+		return 1;
+	}
+
+	return 0;
+}
