778da7f12d42306ab34c4b8829a96798fa8e77c2
Yulia Lushinskaya <ylu@portia.ru>
2016-09-30 17:12:29 +0300

    new ver

--- Lab1/FlipByte/main.cpp
+++ Lab1/FlipByte/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-#include <stdlib.h>
-#include <string>
+#include <cmath>
 using namespace std;
+
+void dectobin(int);
+
+int check;
+
+int out[8];
 
 int main()
 {
-	unsigned int  dec;
-	cin >> dec;
-	for (int i = 0; i<32; i++, dec <<= 1) putchar(48 + (dec >> 31));
+	int q;
+	cin >> q;
+	dectobin(q);
+	system("pause");
+	return 0;
+}
+
+void dectobin(int dc)
+{
+	for (int i = 7; i >= 0; i--)
+	{
+		int c = pow(2.0, i);
+		check = c & dc;
+		if (check)
+		{
+			out[i] = 1;
+		}
+		else out[i] = 0;
+	}
+	for (int i = 0; i<8; i++)
+	{
+		cout << out[i];
+	}
+	cout << endl;
 }
