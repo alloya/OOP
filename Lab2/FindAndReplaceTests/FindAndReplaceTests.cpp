b7d874202cd661f3a0cebb423448f200f433abb9
Yulia Lushinskaya <ylu@portia.ru>
2016-11-03 17:41:47 +0300

    Fixed tests

--- Lab2/FindAndReplaceTests/FindAndReplaceTests.cpp
+++ Lab2/FindAndReplaceTests/FindAndReplaceTests.cpp
@@ -6,39 +6,45 @@
 
 BOOST_AUTO_TEST_SUITE(Test)
 
-BOOST_AUTO_TEST_CASE(Find_And_Replace)
+BOOST_AUTO_TEST_CASE(Replace_Word_In_String)
 {
 	{
-		string subject = "123456789";
-		auto newString = FindAndReplace(subject, "1", "0");
-		const string correctString = "023456789";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "big paws, big black nose, stubby tail, and growl he goes";
-		auto newString = FindAndReplace(subject, "big", "small");
-		const string correctString = "small paws, small black nose, stubby tail, and growl he goes";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "big paws, big black nose, stubby tail, and growl he goes";
-		auto newString = FindAndReplace(subject, "paws", "ears");
-		const string correctString = "big ears, big black nose, stubby tail, and growl he goes";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "1223344455566667777888899999...";
-		auto newString = FindAndReplace(subject, "0", "000");
-		const string correctString = "1223344455566667777888899999...";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "1223344455566667777888899999...";
-		auto newString = FindAndReplace(subject, "6", "0");
-		const string correctString = "1223344455500007777888899999...";
+		string subject = "A bad beginning makes a bad ending.";
+		auto newString = FindAndReplace(subject, "bad", "good");
+		const string correctString = "A good beginning makes a good ending.";
 		BOOST_CHECK(newString == correctString);
 	}
 
+}
+
+BOOST_AUTO_TEST_CASE(Replace_Unexisting_Word_In_String)
+{
+	{
+		string subject = "A bad beginning makes a bad ending.";
+		auto newString = FindAndReplace(subject, "happy", "wonderful");
+		BOOST_CHECK(newString == subject);
+	}
+
+}
+
+BOOST_AUTO_TEST_CASE(Replace_Single_Character_In_String)
+{
+	{
+		string subject = "Hotel";
+		auto newString = FindAndReplace(subject, "H", "M");
+		const string correctString = "Motel";
+		BOOST_CHECK(newString == correctString);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(Replace_Several_Digits_In_String)
+{
+	{
+		string subject = "111222333444";
+		auto newString = FindAndReplace(subject, "111", "000");
+		const string correctString = "000222333444";
+		BOOST_CHECK(newString == correctString);
+	}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
