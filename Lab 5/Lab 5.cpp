a65fb775814a90c52d7dff1250e8314a52eed450
somename <testmail@gmail.com>
2016-12-29 21:39:06 +0300

    Lab5 frame

---
+++ Lab 5/Lab 5.cpp
@@ -0,0 +1,11 @@
+// Lab 5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
---
+++ Lab 5/Lab5Tests/Lab5Tests.cpp
@@ -0,0 +1,45 @@
+//
+
+#include "stdafx.h"
+#include <boost/test/output/compiler_log_formatter.hpp>
+
+
+/*
+������ ����� ��������� ��������������� ������� ������� ������
+��� ����, ����� ������� ���������, ���������� ������ ���� �������� � ������ --log-level=test_suite (��. Post-build event � ���������� �������)
+*/
+class SpecLogFormatter :
+	public boost::unit_test::output::compiler_log_formatter
+{
+	virtual void test_unit_start(std::ostream &os, boost::unit_test::test_unit const& tu) override
+	{
+		// ����� �������� test unit-� ������� ��� test unit-�, ������� ������ ������������� �� ������
+		os << std::string(m_indent, ' ') << boost::replace_all_copy(tu.p_name.get(), "_", " ") << std::endl;
+		// ����������� ������ ��� ������ ���� ����������� test unit-�� � ���� ������
+		m_indent += 2;
+	}
+
+	virtual void test_unit_finish(std::ostream &/*os*/, boost::unit_test::test_unit const& /*tu*/, unsigned long /*elapsed*/) override
+	{
+		// �� ��������� test unit-� ��������� ������
+		m_indent -= 2;
+	}
+
+	int m_indent = 0;
+};
+
+boost::unit_test::test_suite* init_unit_test_suite(int /*argc*/, char* /*argv*/[])
+{
+	// �������� ��� log formatter �� ����������������
+	boost::unit_test::unit_test_log.set_formatter(new SpecLogFormatter);
+	// ��� ��������� ������ ������ - All tests
+	boost::unit_test::framework::master_test_suite().p_name.value = "All tests";
+	return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+	// ��������� �����, ��������� ��������� ��������� ������ � ��������� ������� ������������� ������
+	return boost::unit_test::unit_test_main(&init_unit_test_suite, argc, argv);
+}
