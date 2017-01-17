0b010bdee1b692b5ff31d5d8d230bb4294c0b04b
somename <testmail@gmail.com>
2017-01-17 20:23:21 +0300

    MyArray framework

---
+++ Lab7/MyArray/MyArray.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <new>
+#include <algorithm>
+
+template <typename T>
+class CMyArray
+{
+public:
+	CMyArray() = default;
+
+	CMyArray(const CMyArray& arr)
+	{
+		const auto size = arr.GetSize();
+		if (size != 0)
+		{
+			m_begin = RawAlloc(size);
+			try
+			{
+				CopyItems(arr.m_begin, arr.m_end, m_begin, m_end);
+				m_endOfCapacity = m_end;
+			}
+			catch (...)
+			{
+				DeleteItems(m_begin, m_end);
+				throw;
+			}
+		}
+	}
+
+	void Append(const T & value)
+	{
+		if (m_end == m_endOfCapacity) // no free space
+		{
+			size_t newCapacity = std::max(1u, GetCapacity() * 2);
+
+			auto newBegin = RawAlloc(newCapacity);
+			T *newEnd = newBegin;
+			try
+			{
+				CopyItems(m_begin, m_end, newBegin, newEnd);
+				// Конструируем копию value по адресу newItemLocation
+				new (newEnd)T(value);
+				++newEnd;
+			}
+			catch (...)
+			{
+				DeleteItems(newBegin, newEnd);
+				throw;
+			}
+			DeleteItems(m_begin, m_end);
+
+			// Переключаемся на использование нового хранилища элементов
+			m_begin = newBegin;
+			m_end = newEnd;
+			m_endOfCapacity = m_begin + newCapacity;
+		}
+		else // has free space
+		{
+			new (m_end) T(value);
+			++m_end;
+		}
+	}
+
+	T & GetBack()
+	{
+		assert(GetSize() != 0u);
+		return m_end[-1];
+	}
+
+	const T & GetBack()const
+	{
+		assert(GetSize() != 0u);
+		return m_end[-1];
+	}
+
+	size_t GetSize()const
+	{
+		return m_end - m_begin;
+	}
+
+	size_t GetCapacity()const
+	{
+		return m_endOfCapacity - m_begin;
+	}
+	~CMyArray()
+	{
+		DeleteItems(m_begin, m_end);
+	}
+private:
+	static void DeleteItems(T *begin, T *end)
+	{
+		// Разрушаем старые элементы
+		DestroyItems(begin, end);
+		// Освобождаем область памяти для их хранения
+		RawDealloc(begin);
+	}
+
+	// Копирует элементы из текущего вектора в to, возвращает newEnd
+	static void CopyItems(const T *srcBegin, T *srcEnd, T * const dstBegin, T * & dstEnd)
+	{
+		for (dstEnd = dstBegin; srcBegin != srcEnd; ++srcBegin, ++dstEnd)
+		{
+			// Construct "T" at "dstEnd" as a copy of "*begin"
+			new (dstEnd)T(*srcBegin);
+		}
+	}
+
+	static void DestroyItems(T *from, T *to)
+	{
+		// dst - адрес объект, при конструирование которого было выброшено исключение
+		// to - первый скорнструированный объект
+		while (to != from)
+		{
+			--to;
+			// явно вызываем деструктор для шаблонного типа T
+			to->~T();
+		}
+	}
+
+	static T *RawAlloc(size_t n)
+	{
+		size_t memSize = n * sizeof(T);
+		T *p = static_cast<T*>(malloc(memSize));
+		if (!p)
+		{
+			throw std::bad_alloc();
+		}
+		return p;
+	}
+
+	static void RawDealloc(T *p)
+	{
+		free(p);
+	}
+
+private:
+	T *m_begin = nullptr;
+	T *m_end = nullptr;
+	T *m_endOfCapacity = nullptr;
+};
---
+++ Lab7/MyArrayTests/MyArrayTests.cpp
@@ -0,0 +1,74 @@
+
+#include "stdafx.h"
+#include "../MyArray/MyArray.h"
+
+using namespace std;
+
+struct ArrayItem
+{
+	ArrayItem(int value = 0) : value(value)
+	{}
+	int value;
+};
+
+struct EmptyStringArray
+{
+	CMyArray<ArrayItem> arr;
+};
+
+BOOST_FIXTURE_TEST_SUITE(MyArray, EmptyStringArray)
+	BOOST_AUTO_TEST_SUITE(by_default)
+		BOOST_AUTO_TEST_CASE(is_empty)
+		{
+			BOOST_CHECK_EQUAL(arr.GetSize(), 0u);
+		}
+		BOOST_AUTO_TEST_CASE(has_0_capacity)
+		{
+			BOOST_CHECK_EQUAL(arr.GetCapacity(), 0u);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(after_appending_an_item)
+		BOOST_AUTO_TEST_CASE(increases_its_size_and_capacity)
+		{
+			arr.Append(ArrayItem());
+			BOOST_CHECK_EQUAL(arr.GetSize(), 1u);
+			BOOST_CHECK_EQUAL(arr.GetCapacity(), 1u);
+			arr.Append(ArrayItem());
+			BOOST_CHECK_EQUAL(arr.GetSize(), 2u);
+			BOOST_CHECK_EQUAL(arr.GetCapacity(), 2u);
+			arr.Append(ArrayItem());
+			BOOST_CHECK_EQUAL(arr.GetSize(), 3u);
+			BOOST_CHECK_EQUAL(arr.GetCapacity(), 4u);
+			arr.Append(ArrayItem());
+			BOOST_CHECK_EQUAL(arr.GetSize(), 4u);
+			BOOST_CHECK_EQUAL(arr.GetCapacity(), 4u);
+		}
+		BOOST_AUTO_TEST_CASE(it_becomes_available_at_the_back)
+		{
+			arr.Append(1);
+			BOOST_CHECK_EQUAL(arr.GetBack().value, 1);
+			arr.Append(2);
+			BOOST_CHECK_EQUAL(arr.GetBack().value, 2);
+			arr.Append(3);
+			BOOST_CHECK_EQUAL(arr.GetBack().value, 3);
+			arr.Append(4);
+			BOOST_CHECK_EQUAL(arr.GetBack().value, 4);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(after_copy_construction)
+		BOOST_AUTO_TEST_CASE(has_size_capacity_equal_to_size_of_original_array)
+		{
+			for (auto i = 0; i < 6; ++i)
+			{
+				arr.Append(i);
+			}
+			BOOST_CHECK_NE(arr.GetSize(), arr.GetCapacity());
+
+			auto copy(arr);
+			BOOST_CHECK_EQUAL(copy.GetSize(), arr.GetSize());
+			BOOST_CHECK_EQUAL(copy.GetCapacity(), arr.GetSize());
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
