570abf1908debd6598dd4b7577391de2de102032
somename <testmail@gmail.com>
2017-01-26 20:50:27 +0300

    mystack

--- Lab7/MyArray/MyArray.h
+++ Lab7/MyArray/MyArray.h
@@ -1,138 +1,76 @@
 #pragma once
 
 template <typename T>
-class CMyArray
+class CMyStack
 {
 public:
-	CMyArray() = default;
+	CMyStack() = default;
 
-	CMyArray(const CMyArray& arr)
+	CMyStack(CMyStack<T> const &stack)
 	{
-		const auto size = arr.GetSize();
-		if (size != 0)
+		*this = stack;
+	}
+
+	CMyStack(CMyStack<T> &&stack)
+	{
+		*this = std::move(stack);
+	}
+
+	~CMyStack()
+	{
+		Clear();
+	}
+
+	bool IsEmpty()
+	{
+		return (m_size == 0);
+	}
+
+	void Pop()
+	{
+		if (IsEmpty())
 		{
-			m_begin = RawAlloc(size);
-			try
-			{
-				CopyItems(arr.m_begin, arr.m_end, m_begin, m_end);
-				m_endOfCapacity = m_end;
-			}
-			catch (...)
-			{
-				DeleteItems(m_begin, m_end);
-				throw;
-			}
+			trow std::underflow_error("Stack is empty.");
+		}
+		m_last = m_last->prevNode;
+	}
+
+	void Clear()
+	{
+		while (!IsEmpty())
+		{
+			Pop();
 		}
 	}
 
-	void Append(const T & value)
+	void Push(const T &element)
 	{
-		if (m_end == m_endOfCapacity) // no free space
+		auto newElement = std::make_shared<Node>();
+		newElement->data = element;
+		if (m_last != nullptr)
 		{
-			size_t newCapacity = std::max(1u, GetCapacity() * 2);
-
-			auto newBegin = RawAlloc(newCapacity);
-			T *newEnd = newBegin;
-			try
-			{
-				CopyItems(m_begin, m_end, newBegin, newEnd);
-				// Конструируем копию value по адресу newItemLocation
-				new (newEnd)T(value);
-				++newEnd;
-			}
-			catch (...)
-			{
-				DeleteItems(newBegin, newEnd);
-				throw;
-			}
-			DeleteItems(m_begin, m_end);
-
-			// Переключаемся на использование нового хранилища элементов
-			m_begin = newBegin;
-			m_end = newEnd;
-			m_endOfCapacity = m_begin + newCapacity;
+			newElement->prevNode = m_last;
 		}
-		else // has free space
+		m_last = newElement;
+		++m_size;
+	}
+
+	T GetLastElement()const
+	{
+		if (IsEmpty())
 		{
-			new (m_end) T(value);
-			++m_end;
+			trow std::underflow_error("Stack is empty.");
 		}
-	}
-
-	T & GetBack()
-	{
-		assert(GetSize() != 0u);
-		return m_end[-1];
-	}
-
-	const T & GetBack()const
-	{
-		assert(GetSize() != 0u);
-		return m_end[-1];
-	}
-
-	size_t GetSize()const
-	{
-		return m_end - m_begin;
-	}
-
-	size_t GetCapacity()const
-	{
-		return m_endOfCapacity - m_begin;
-	}
-	~CMyArray()
-	{
-		DeleteItems(m_begin, m_end);
-	}
-private:
-	static void DeleteItems(T *begin, T *end)
-	{
-		// Разрушаем старые элементы
-		DestroyItems(begin, end);
-		// Освобождаем область памяти для их хранения
-		RawDealloc(begin);
-	}
-
-	// Копирует элементы из текущего вектора в to, возвращает newEnd
-	static void CopyItems(const T *srcBegin, T *srcEnd, T * const dstBegin, T * & dstEnd)
-	{
-		for (dstEnd = dstBegin; srcBegin != srcEnd; ++srcBegin, ++dstEnd)
-		{
-			// Construct "T" at "dstEnd" as a copy of "*begin"
-			new (dstEnd)T(*srcBegin);
-		}
-	}
-
-	static void DestroyItems(T *from, T *to)
-	{
-		// dst - адрес объект, при конструирование которого было выброшено исключение
-		// to - первый скорнструированный объект
-		while (to != from)
-		{
-			--to;
-			// явно вызываем деструктор для шаблонного типа T
-			to->~T();
-		}
-	}
-
-	static T *RawAlloc(size_t n)
-	{
-		size_t memSize = n * sizeof(T);
-		T *p = static_cast<T*>(malloc(memSize));
-		if (!p)
-		{
-			throw std::bad_alloc();
-		}
-		return p;
-	}
-
-	static void RawDealloc(T *p)
-	{
-		free(p);
+		return m_last->data;
 	}
 
 private:
-	T *m_begin = nullptr;
-	T *m_end = nullptr;
-	T *m_endOfCapacity = nullptr;
+	struct Node
+	{
+		T data = T();
+		std::shared_ptr<Node> prevNode = nullptr;
+	};
+
+	size_t m_size;
+	std::shared_ptr<Node> m_last = nullptr;
 };
